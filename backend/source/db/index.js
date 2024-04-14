1f83b80a4ba1bac8e680d93591cd89ac1992f8ef
yusufazam225 <justforcoding225@gmail.com>
2024-04-14 23:49:49 +0530

    4th day

---
+++ backend/source/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB=async()=>{
+    try{
+        const connectmongo=await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`host is :${connectmongo.connection.host}`);
+    }catch(error)
+    {
+        console.log(`the error is in connection:${error}`);
+        process.exit(1);
+    }
+}
+export default connectDB;
