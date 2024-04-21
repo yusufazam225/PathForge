0778f5c690388ee2fe42b71191539a41a9405082
yusufazam225 <justforcoding225@gmail.com>
2024-04-22 02:11:06 +0530

    8th day

--- backend/source/routes/smtp.js
+++ backend/source/routes/smtp.js
@@ -7,12 +7,12 @@ router.post('/smtp',(req,res)=>{
         const {email,otp}=req.body;
        
         const transporter = nodemailer.createTransport({
-            host: `${process.env.SMTP_HOST}`,
+            host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
             secure: false, // Use `true` for port 465, `false` for all other ports
             auth: {
-              user: `${process.env.SMTP_USER}`,
-              pass: `${process.env.SMTP_PASS}`,
+              user: process.env.SMTP_USER,
+              pass: process.env.SMTP_PASS,
             },
           });
           
