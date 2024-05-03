c2c7b9636f18159b83dd743e085eeb3978d0cf3c
yusufazam225 <justforcoding225@gmail.com>
2024-05-03 20:15:59 +0530

    modifications

--- backend/source/index.js
+++ backend/source/index.js
@@ -14,7 +14,7 @@ dotenv.config({
 })
 
 const app=express();
-app.use(cors({origin:'http://localhost:3000',credentials:true}));
+app.use(cors());
 app.use(cookieParser());
 
 app.use(express.json())
